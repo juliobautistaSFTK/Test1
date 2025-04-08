c62db3b7beba5160a9eb9fb5f875949ce6ff79a1
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2025-04-08 16:09:45 -0600

    Update DatabaseConnection.java

--- src/main/java/com/softtek/util/DatabaseConnection.java
+++ src/main/java/com/softtek/util/DatabaseConnection.java
@@ -8,12 +8,12 @@ public class DatabaseConnection {
 	private static final String URL = "jdbc:mariadb://mariadb_1:3306/taskmanager";
 	private static final String USER = "root";
 	private static final String PASSWORD = "12345678";
-	
+
 	public static Connection getConnection() throws SQLException {
 		Connection conn =null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
 		} catch (ClassNotFoundException e) {
 			throw new SQLException("Error cargando el driver JDBC de MariaDB", e);
 		       
